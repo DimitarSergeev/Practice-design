19a16968a8f79c325b474a7d43c4d1f6e3341833
Dimitar Sergev <102225734+DimitarSergeev@users.noreply.github.com>
2023-01-21 15:20:47 +0200

    Add styles on product-Chair

--- design-practice/src/components/ProductChair/ProductChair.js
+++ design-practice/src/components/ProductChair/ProductChair.js
@@ -1,39 +1,18 @@
 import styles from './ProductChair.module.css'
 
-import chairIMG from './chairImg.png'
+import { SwiperChairs } from './SwiperChairs'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination } from 'swiper';
-
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 export const ProductChair = () => {
     return (
         <article className={styles.container} >
+
             <div className={styles['img-container']}>
-                <Swiper
-                    modules={[Pagination]}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    navigation
-                    pagination={{ clickable: true }}
-                    // scrollbar={{ draggable: true }}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    onSlideChange={() => console.log('slide change')}
-                >
-                    <SwiperSlide >
-                        <img src={chairIMG} alt="nqma" />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <img src={chairIMG} alt="nqma" />
-                    </SwiperSlide>
-                    <SwiperSlide >
-                        <img src={chairIMG} alt="nqma" />
-                    </SwiperSlide>
-                </Swiper>
+                <span className={styles.code}>RT34FY</span>
+                <SwiperChairs className={styles['swiper-conainer']} />
+                <div className="swiper-pagination"></div>
             </div>
+
             <div className={styles['info-container']}>
                 <div className={styles['basic-info']}>
                     <span className={styles.type}>Chairs</span>
@@ -42,11 +21,11 @@ export const ProductChair = () => {
                     <div className={styles['price-rating']}>
                         <h2 className={styles.price}>$420</h2>
                         <div className={styles['rating-icons']}>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
                         </div>
                     </div>
                 </div>
@@ -60,26 +39,35 @@ export const ProductChair = () => {
                         </div>
                         <p className={styles.desc}> Lorem ipsum dolor sit amet, consectetur
                             adipiscing elit. Sed sagittis quam ac nulla blandit
-                            vehicula at vitae ipsum... read more
+                            vehicula at vitae ipsum
                         </p>
                     </div>
 
                     <div className={styles['order-info']}>
-                        <div className={styles['color-container']}>
+                        <div className={styles['order-container']}>
                             <label htmlFor="color">COLOR</label>
-                            <select name="color" id="color">
-                                <option value="yellow">Yellow</option>
-                                <option value="green">Green</option>
-                                <option value="red">Red</option>
-                            </select>
+                            <div className={styles['color-container']}>
+                                <span className={styles.color}></span>
+                                <select name="color" id="color">
+                                    <option value="yellow" style={{ "background-color": "#ffff00" }}></option>
+                                    <option value="green" style={{ "background-color": "#00ff00" }}></option>
+                                    <option value="#ff0000" style={{ "background-color": "#ff0000" }}></option>
+                                </select>
+                            </div>
                         </div>
-                        <div className={styles['color-container']}>
-                            <label htmlFor="color">QUANTITY</label>
-                            <input type="text" defaultValue='1' />
+                        <div className={styles['order-container']}>
+                            <label htmlFor="quant">QUANTITY</label>
+                            <div className={styles['quantity-container']} id='quant'>
+                                <button className={styles['quantity-btn']}>-</button>
+                                <p className={styles.pices}>2</p>
+                                <button className={styles['quantity-btn']}>+</button>
+                            </div>
                         </div>
-                        <div className={styles['color-container']}>
+                        <div className={styles['order-container']}>
                             <label htmlFor="totlaPrice">TOTAL PRICE</label>
-                            <span className={styles['total-price']} id='totlaPrice'>$840</span>
+                            <div className={styles['total-container']}>
+                                <span className={styles['total-price']} id='totlaPrice'>$840</span>
+                            </div>
                         </div>
                     </div>
 
---
+++ design-practice/src/components/ProductChair/SwiperChairs.js
@@ -0,0 +1,43 @@
+import styles from './ProductChair.module.css'
+
+import chairIMG from './chairImg.png'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/grid';
+
+export const SwiperChairs = () => {
+    return (
+        <div >
+
+            <Swiper
+                loop={true}
+                modules={[Pagination]}
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                className={styles['swiper-conainer']}
+                // scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+
+                <SwiperSlide key={1}>
+                    <img src={chairIMG} alt="nqma" />
+                </SwiperSlide>
+                <SwiperSlide key={2}>
+                    <img src={chairIMG} alt="nqma" />
+                </SwiperSlide>
+                <SwiperSlide key={3}>
+                    <img src={chairIMG} alt="nqma" />
+                </SwiperSlide>
+
+
+              
+            </Swiper>
+        </div>
+    )
+}
